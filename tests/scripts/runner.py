52fb37a83bc6001f5892efd75761b21f445ada53
Bryce Morrow <brycemorrow4564@gmail.com>
2022-01-14 19:36:23 -0500

    debug action

--- tests/scripts/runner.py
+++ tests/scripts/runner.py
@@ -2,5 +2,5 @@ import subprocess
 
 
 def run_tests():
-    subprocess.run("pytest ./tests".split(" "))
+    subprocess.run("pwd && ls && pytest ./tests".split(" "))
     # subprocess.run("pytest -p no:cacheprovider tests ".split(" "))
