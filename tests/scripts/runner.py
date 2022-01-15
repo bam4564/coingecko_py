d0cd32aba871108afa3685935b6a5646d86c62ee
Bryce Morrow <brycemorrow4564@gmail.com>
2022-01-14 19:47:37 -0500

    lol i forgot the test file

--- tests/scripts/runner.py
+++ tests/scripts/runner.py
@@ -2,8 +2,4 @@ import subprocess
 
 
 def run_tests():
-    subprocess.run(["ls", "."])
-    subprocess.run(["echo", "'\n'"])
-    subprocess.run(["ls", "./tests"])
     subprocess.run("poetry run pytest ./tests -vvv".split(" "))
-    # subprocess.run("pytest -p no:cacheprovider tests ".split(" "))
