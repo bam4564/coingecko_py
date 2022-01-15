e880bb3d2372dda184815b31f53c9a6cbc00231c
Bryce Morrow <brycemorrow4564@gmail.com>
2022-01-14 19:44:57 -0500

    debug

--- tests/scripts/runner.py
+++ tests/scripts/runner.py
@@ -3,5 +3,7 @@ import subprocess
 
 def run_tests():
     subprocess.run(["ls", "."])
-    subprocess.run("poetry run pytest ./tests".split(" "))
+    subprocess.run(["echo", "'\n'"])
+    subprocess.run(["ls", "./tests"])
+    subprocess.run("poetry run pytest ./tests -vvv".split(" "))
     # subprocess.run("pytest -p no:cacheprovider tests ".split(" "))
