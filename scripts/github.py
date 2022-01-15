9d4ce9db66d764cdfe832b92a9f905159aa92e55
Bryce Morrow <brycemorrow4564@gmail.com>
2022-01-15 00:01:35 -0500

    cicd

--- scripts/github.py
+++ scripts/github.py
@@ -1,12 +1,10 @@
-import math 
-import re 
+import math
+import re
 
-def get_cov_percent(): 
-    with open("cov.xml", 'r') as f: 
+
+def get_cov_percent():
+    with open("cov.xml", "r") as f:
         lines = f.readlines()
     match = float(re.findall(r'line-rate="([^"]*)"', lines[1])[0])
     percent = math.ceil(match * 100)
     print(percent)
-
-    
-        
