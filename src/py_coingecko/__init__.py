e9553bb380033f823e9db8599d1a6c0432fbd458
Bryce Morrow <brycemorrow4564@gmail.com>
2022-01-16 22:06:58 -0500

    THings are gud

--- src/py_coingecko/__init__.py
+++ src/py_coingecko/__init__.py
@@ -1,4 +1,7 @@
-try:
-    from .py_coingecko import CoingeckoApi, error_msgs
-except:
-    pass
+from .py_coingecko import CoingeckoApi, error_msgs
+
+# Uncomment when generating a client and no existing client 
+# try: 
+#     from .py_coingecko import CoingeckoApi, error_msgs
+# except: 
+#     pass 
